3329790e2a93f37f21c84b4307e2c276bbcc60a5
mmartinde <manuel.delamontana@bootcamp-upgrade.com>
2024-01-27 08:49:24 +0100

    Home page

--- src/pages/index/Index.jsx
+++ src/pages/index/Index.jsx
@@ -1,10 +1,15 @@
-function Index (){
-    return (
-        <>
-            <h1> Index</h1>
-        </>
-    )
+import React from 'react';
+
+function Index() {
+  return (
+    <>
+      <div className="home">
+      <h1>Welcome to the Movie Database</h1>
+      <p>Explore our collection of movies</p>
+      <img src="https://i0.wp.com/imgs.hipertextual.com/wp-content/uploads/2017/03/Captura-de-pantalla-2017-03-30-a-las-11.59.05.png?w=2012&quality=70&strip=all&ssl=1" alt="Movie background" style={ {width:'800px', height: '600px'}} />
+    </div>
+    </>
+  );
 }
 
-
-export default Index
+export default Index;
